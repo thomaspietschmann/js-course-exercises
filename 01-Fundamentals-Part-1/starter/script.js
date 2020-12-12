d1dffa97c96fc86a45464c49a2b45f11e009933c
thomaspietschmann <thomas@pietschi.com>
2020-12-12 19:28:41 +0100

    still easy

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,54 @@
+/* let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof true);
+console.log(typeof jsIsFun);
+console.log("23 is a " + typeof 23);
+console.log("Thomas is a " + typeof "Thomas");
+
+jsIsFun = "Yes";
+
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // regarded as a bug, should return null
+
+let age = 33;
+age = 31;
+
+const birthYear = 1980;
+// birthYear = 1981; // assignment to constant variable error
+
+*/
+
+const myAge = 40;
+const yourAge = 39;
+console.log(myAge - 3, yourAge / 2 ** 3); // 2 ** 3 means to to the power of 3 = 2 * 2 * 2
+console.log(myAge < yourAge);
+
+const isFullAge = myAge >= 18;
+console.log(isFullAge);
+
+
+x = 5;
+x += 10;
+x -= 1;
+x++;
+x--;
+x *= 4;
+console.log(x);
+
+
+
+console.log("This is the last line.");
+
+
+
+// const job; // error
