6c9ce550dd93b45ff79344a663639f5ddd8e9dcf
Thomas Pietschmann <thomas@pietschi.com>
2020-12-14 13:03:37 +0100

    finish chapter 2, Fundamentals part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -147,7 +147,6 @@ n = n - 1;
 console.log(n);
 
 
-*/
 
 
 // false = not really false, but will false when converted to boolean
@@ -164,6 +163,117 @@ if (money) {
 } else {
   console.log("You should get a job.")
 }
-
-
 //truthy =
+
+const age = 18;
+if (age === 18) console.log("You jsut became an adult");
+// === strict
+// == loose
+// use === as default to avoid bugs!
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+
+if (favourite === 23) {
+  console.log("23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also an awesome number");
+} else {
+  console.log("not such a great number...");
+}
+
+if (favourite !== 23) {
+  console.log("Why not try 23?");
+}
+// !== different operator
+
+
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense && hasGoodVision);
+console.log(!hasDriversLicense || hasGoodVision);
+
+const isTired = true;
+console.log(hasDriversLicense && hasGoodVision && !isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive");
+}
+
+
+const scoreDolphins = (150 + 1 + 89) / 3;
+const scoreKoalas = (150 + 1 + 89) / 3;
+
+if (scoreDolphins >= 100 && scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win!");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Koalas win!");
+} else if (scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log("It's a tie!");
+} else {
+  console.log("Nobody wins...");
+}
+
+// Switch statement
+
+const day = "wednesday";
+
+switch (day) {
+  case "monday":
+    console.log("go to work...");
+    console.log("go to coding meetup...");
+    break;
+    case "tuesday":
+      console.log("Prepare food...")
+      break;
+      case "wednesday":
+        case "thursday":
+          console.log("Write code examples");
+          break;
+          default:
+            console.log("Not a valid day");
+          }
+
+
+          // expressions result in values or booleans:
+          3 + 4
+          1991
+          true && false && !false
+          -- > result to true or false
+
+          //statements result into actions
+          if (23 > 10) {
+            console.log("23 is bigger"); // string is an expression
+          }
+
+          const age = 23;
+          age >= 18 ? console.log("I like beer.") : console.log("I should not drink.");
+
+          const drink = age >= 18 ? "wine" : "water";
+          console.log(drink);
+
+          let drink2;
+          if (age >= 18) {
+            drink2 = "wine";
+          } else {
+            drink2 = "water";
+          }
+          console.log(drink2);
+
+          // ternary operator is an expression!
+
+          console.log(`I like to drink ${age >= 18 ? "beer" : "wine"}`)
+
+          */
+const bill = 40;
+const tip = bill >= 50 && bill <= 300 ? 0.15 : 0.20;
+console.log(tip);
+
+console.log(`The bill was ${bill}, the tip was ${bill * tip}, and the total value ${bill + tip}`)
+
