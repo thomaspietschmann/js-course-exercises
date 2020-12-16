d8adee3468937e73ddad26644b5629ed42f43338
Thomas Pietschmann <thomas@pietschi.com>
2020-12-16 14:25:29 +0100

    start guess-my-number project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 100;
+const numberInput = document.querySelector(".guess");
+// console.log(document.querySelector(".guess").value);
+
+const button = document.querySelector(".check");
+button.addEventListener("click", () => console.log(numberInput.value));
