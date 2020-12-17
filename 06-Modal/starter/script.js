aa530424a8f4c69c7e9c879b130581f949a9fac2
Thomas Pietschmann <thomas@pietschi.com>
2020-12-17 10:31:16 +0100

    finish chapter 7 - 81

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -20,3 +20,9 @@ btnsOpenModal.forEach(button => {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
