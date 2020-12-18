0d2039a1ff50fab5531074ac5819a3b85b1d28c8
Thomas Pietschmann <thomas@pietschi.com>
2020-12-18 18:46:34 +0100

    finish chapter 8

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -84,7 +84,6 @@ console.log(x === window.x); // true
 console.log(y === window.y); // false
 console.log(z === window.z); // false
 
-*/
 // this keyword
 
 console.log(this);
@@ -124,3 +123,95 @@ matilda.calcAge();
 // f(); // undefined
 const f = thomas;
 f.calcAge();
+
+
+// normal vs arrow functions
+
+const thomas = {
+  year: 1983,
+  firstName: "Thomas",
+  calcAge: function () {
+    console.log(2020 - this.birthYear);
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    }
+    isMillenial();
+  },
+}
+
+thomas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+
+}
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+// Primitives (numbers, strings, booleans)
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: "Jonas",
+  age: 30
+}
+
+const friend = me;
+
+friend.age = 27;
+
+console.log("Friend", friend);
+console.log("Me", me);
+
+// Primitive = primitive type
+// objects = reference types
+// All objects get saved in the HEAP
+// primitives are stored in the call stack
+
+//primitive type
+let lastName = "Williams";
+let oldLastName = "Williams";
+lastName = "Burgers";
+console.log(lastName);
+console.log(oldLastName);
+
+//reference type
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = "Burgers";
+console.log(jessica);
+console.log(marriedJessica);
+
+*/
+//copying objects
+
+const jessica2 = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alice", "Bob"]
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = "Burgers";
+console.log(jessica2);
+console.log(jessicaCopy);
+// Only works on first level! Object.assign only creates a "shallow copy", not a "deep clone"
+
+jessicaCopy.family.push("Mary");
+jessicaCopy.family.push("Hugo");
+console.log(jessica2);
+console.log(jessicaCopy);
