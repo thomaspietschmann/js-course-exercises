27883f35b2d3e224a8f9e6b76b74c0e199b1cead
Thomas Pietschmann <thomas@pietschi.com>
2020-12-18 13:57:22 +0100

    chapter 8 - 97

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+
 function calcAge(birthYear) {
   const age = 2020 - birthYear;
 
@@ -33,3 +35,92 @@ function calcAge(birthYear) {
 
 const firstName = "Thomas";
 calcAge(1982);
+
+
+// Hoisting
+
+// Variables
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Thomas";
+let job = "happiness manager";
+const year = 1980;
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr)(2, 3);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+
+if (!numProducts) deleteShoppingCart();
+// numProducts is undefined, therefore function is called! Not wanted!
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted.");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
+
+*/
+// this keyword
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2020 - birthYear);
+  console.log(this); // undefined
+}
+calcAge(1980);
+
+const calcAgeArrow = birthYear => {
+  console.log(2020 - birthYear);
+  console.log(this); // window, lexical this
+}
+calcAgeArrow(1981);
+
+const thomas = {
+  birthYear: 1980,
+  calcAge: function () {
+    console.log(2020 - this.birthYear);
+  }
+}
+
+thomas.calcAge();
+
+
+const matilda = {
+  birthYear: 2017
+};
+
+// method borrowing
+matilda.calcAge = thomas.calcAge;
+console.log(matilda);
+matilda.calcAge();
+
+// const f = thomas.calcAge;
+// f(); // undefined
+const f = thomas;
+f.calcAge();
