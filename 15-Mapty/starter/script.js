af3efb9f61e951b380d9eafd5738b0c14a5adf55
Thomas Pietschmann <thomas@pietschi.com>
2021-01-01 20:30:42 +0100

    chapter 15 - 236

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,6 +3,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10); // usually handled by library
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords;
@@ -18,6 +19,9 @@ class Workout {
     this.description = `${this.type[0].toUpperCase() + this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+  click() {
+    this.clicks++;
   }
 }
 
@@ -69,6 +73,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
@@ -78,6 +83,8 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
+
+    containerWorkouts.addEventListener('click', this._movetoPopUp.bind(this));
   }
 
   _getPosition() {
@@ -95,7 +102,7 @@ class App {
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -258,8 +265,24 @@ class App {
 
     form.insertAdjacentHTML('afterend', html);
   }
+  _movetoPopUp(e) {
+    const workoutEl = e.target.closest('.workout');
+    if (!workoutEl) return; // guard clause
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    workout.click();
+  }
 }
 
-// navigator.geolocation.getCurrentPosition(callbackSuccess, callbackError);
-
 const app = new App();
+
+// navigator.geolocation.getCurrentPosition(callbackSuccess, callbackError);
