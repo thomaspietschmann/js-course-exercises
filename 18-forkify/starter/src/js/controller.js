bd91538d1b288f029a0f775c573d4ea6ec4e1d08
Thomas Pietschmann <thomas@pietschi.com>
2021-01-05 23:57:16 +0100

    chapter 18 - 280

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -30,10 +30,12 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     // 1 Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} ${res.status}`);
@@ -159,3 +161,8 @@ ${recipe.ingredients
 };
 
 showRecipe();
+
+// handle change of URL id and show recipe on load
+['hashchange', 'load'].forEach(ev => {
+  window.addEventListener(ev, showRecipe);
+});
